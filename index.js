59162c4c2aa9a1a06ff0453ac371ac8b836bfda3
hendratay <hendratay96@gmail.com>
2019-02-20 01:11:43 +0700

    change cron job time

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const http = require("http");
 const CronJob = require("cron").CronJob;
 
-const job = new CronJob("0 */15 6-12 * * *", function() {
+const job = new CronJob("0 */15 8-21 * * *", function() {
   http.get(process.env.SERVER);
+  console.log(`ping ${process.env.SERVER}`);
 });
 
 job.start();
+console.log("job is running");
 
