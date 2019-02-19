9272e864a86b66899eb1e510709ec51721f58323
hendratay <hendratay96@gmail.com>
2019-02-20 00:34:13 +0700

    only keep server alive

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const http = require("http");
 const CronJob = require("cron").CronJob;
 
 const job = new CronJob("0 */15 6-12 * * *", function() {
-  http.get(process.env.FRONT_END);
-  http.get(process.env.BACK_END);
+  http.get(process.env.SERVER);
 });
+
 job.start();
+
