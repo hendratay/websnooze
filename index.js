71f83b572830b7791abf76caa777839e900f38f1
hendratay <hendratay96@gmail.com>
2019-02-20 23:05:47 +0700

    create server

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const https = require("https");
 const CronJob = require("cron").CronJob;
 
+const server = https.createServer((req, res) => {
+  res.end();
+}).listen(process.env.PORT);
+
 const job = new CronJob("0 */15 8-21 * * *", function() {
   https.get(process.env.SERVER);
   console.log(`ping ${process.env.SERVER}`);
